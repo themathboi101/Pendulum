ab2dc31858ca3b640a4758b9cdfcfebf9615d0e4
themathboi101 <78664310+themathboi101@users.noreply.github.com>
2021-06-10 13:04:39 -0400

    Update Roof.js

--- Roof.js
+++ Roof.js
@@ -1,26 +1,17 @@
-class Rope{
-    constructor(bodyA,bodyB,offsetX,offsetY){
-        this.offsetX=offsetX;
-        this.offsetY=offsetY
+class Roof{
+    constructor(x,y,width,height){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
 
-        var options={
-        bodyA:bodyA,
-        bodyB:bodyB,
-        pointB:{x:this.offsetX,y:this.offsetY},
-        }
-
-        this.roof= Matter.Constraint.create(options);
+        
+        this.roof= Bodies.rectangle(x,y,width,height);
         World.add(world,this.roof);
     }
 
     display(){
-        var firstPos=this.roof.bodyA.position;
-        var secondPos=this.roof.bodyB.position;
-        var thirdPos=pointB.x+this.offsetX;
-        var fourthPos=pointB.y+this.offsetY;
-
-        strokeWeight(5);
-        line(firstPos,secondPos,thirdPos,fourthPos);
-
+       rectMode(CENTER);
+        rect(x,y,width,height);
     }
 }
